e99b2e34b54feb7844f0820958211f9893bf07f3
justcami <33912789+justcami@users.noreply.github.com>
2017-11-22 23:41:40 -0500

    Update agendas.php

--- agendas.php
+++ agendas.php
@@ -1,14 +1,43 @@
 <?php
+error_reporting (0);
 include("seguridad.php");
 include ('conexion.php');
 ?>
 <?php 
-            echo "<b>Bienvenido: </b>";
-            echo $user."<br>";
-            ?>
-            <a href="logout.php">Cerrar Sesion</a><br>
-			<a href="main.php">Regresar al inicio</a><br><br>
-<!DOCTYPE html>
+    echo "<b>Bienvenido: </b>";
+    echo $user."<br>";
+?>
+ 
+<?php  
+
+//$connect = mysql_connect("localhost","root",""); 
+//mysql_select_db("contacts",$connect);
+
+//if ($_FILES[csv][size] > 0) { 
+
+//    $file = $_FILES[csv][tmp_name]; 
+//    $handle = fopen($file,"r"); 
+     
+//    do { 
+//        if ($data[0]) { 
+//            mysql_query("INSERT INTO contacts (contact_first, contact_last, contact_email) VALUES 
+//                ( 
+//                    '".addslashes($data[0])."', 
+//                    '".addslashes($data[1])."', 
+//                    '".addslashes($data[2])."' 
+//                ) 
+//            "); 
+//        } 
+//    } while ($data = fgetcsv($handle,1000,",","'")); 
+//    header('Location: agendas.php?success=1'); die; 
+
+//} 
+
+?>
+    <a href="logout.php">Cerrar Sesion</a><br>
+	<a href="main.php">Regresar al inicio</a><br><br>
+
+	<!DOCTYPE html>
 <html lang="es"> 
 <head>
 	<meta charset="utf-8">
@@ -16,7 +45,8 @@ include ('conexion.php');
 
 </head>
 <body>
-	<form action="lista.php" name="Seleccion de lista" id="formulariolista">
+<?php if (!empty($_GET[success])) { echo "<b>Your file has been imported.</b><br><br>"; }//generic success notice ?>
+<form action="" method="post" enctype="multipart/form-data" name="Selecciondelista" id="formlista"> 
 		Archivo: <input name="csv" type="file" id="csv" /> <br><br>
 		Producto: <select name="Producto" id="iddeproducto"> 
         <option value="0">Eliga su producto:</option>
