884f1fbd1eff6ce387ef0ad922071106728e1bb8
justcami <33912789+justcami@users.noreply.github.com>
2017-11-23 03:28:56 -0500

    Update guardarproductos.php

--- guardarproductos.php
+++ guardarproductos.php
@@ -4,20 +4,26 @@ include ('conexion.php');
 
 $producto = $_POST['NombredeProducto'];
 $descripcion = $_POST['Descripcion'];
-
 $ins = $con -> query ("INSERT INTO productos (idproducto,Producto,Descripcion) VALUES ('','$producto','$descripcion')");
 
-if ($ins) {
+$enlace = mysql_connect('localhost','root','');
+mysql_select_db('sepfin',$enlace);
+$tabla = "create table ".$producto."(
+idproducto int(11) PRIMARY KEY AUTO_INCREMENT,
+Nombre varchar(200) NOT NULL,
+Apellido varchar(200) NOT NULL,
+Telefono varchar(12) NOT NULL);";
+$crear_tabla =mysql_query($tabla,$enlace);
+if ($crear_tabla) {
 
 echo "<script>
-        alert('Producto Asignado Exitosamente');
-        location.href='index.php';
+        alert('Producto Creado Exitosamente');
+        location.href='main.php';
         </script>";
 }else{
     echo "<script>
-        alert('No se pudo asignar el producto');
-        location.href='index.php';
+        alert('No se pudo crear el producto');
+        location.href='main.php';
         </script>"; 
 }
-
 ?>
