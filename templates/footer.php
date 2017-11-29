53ecf14fe615d4aa85727c3f33fe068950853622
justcami <33912789+justcami@users.noreply.github.com>
2017-11-28 19:37:43 -0500

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,3 @@
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<hr>
+<center>@Copyright 2017<br />Todos los derechos reservados Axtrac SAS</center>
---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Crear filtros de Productos en php y mysql</title>
+<link rel="stylesheet" type="text/css" href="resources/style.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="#">MENU 1</a></li>
+			<li><a href="#">MENU 1</a></li>
+			<li><a href="#">MENU 1</a></li>
+			<li><a href="#">MENU 1</a></li>
+			<li><a href="#">MENU 1</a></li>
+		</ul>
+	</nav>
